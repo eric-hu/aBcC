f937fea0749d0041f33d0d115f479f92ef38c299
Eric Hu <erichu@Erics-MacBook-Pro.local>
2014-04-22 22:30:46 -0400

    Add prism test-autorunner as a project dependency

--- project.clj
+++ project.clj
@@ -3,5 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [com.aphyr/prism "0.1.1"]]
+
+  :plugins      [[com.aphyr/prism "0.1.1"]]
   :main abcc.core)
