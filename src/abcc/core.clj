49fe52fa999735a5c829eb9a86b76a120eed7eee
Eric Hu <erichu@Erics-MacBook-Pro.local>
2014-05-14 13:33:59 -0400

    Add integer read behavior to read-bencode

--- src/abcc/core.clj
+++ src/abcc/core.clj
@@ -8,11 +8,34 @@
   (alter-var-root #'*read-eval* (constantly false))
   (println "Hello, World!"))
 
-
 (defn read-torrent [filename]
   (slurp filename)
   )
 
 (defn read-bencode [string]
-  1
+  " "
   )
+
+(defn read-bencoded-integer [string]
+  (let [[number-as-string remainder-string] (clojure.string/split string #"e" 2)]
+    (if (or
+          ; check if string is \e terminated
+          (and (= 0 (count remainder-string))
+               (not (= \e (last string))))
+          ; check if string is non-zero and begins with 0
+          (and (> (count number-as-string) 1)
+               (= \0 (first number-as-string)))
+          ; check if string is negative-prefixed zero
+          (or (and (= (count number-as-string) 2)
+                   (= \- (first number-as-string))
+                   (= \0 (nth number-as-string 1)))
+              (and (> (count number-as-string) 2)
+                   (= \- (first number-as-string))
+                   (= \0 (nth number-as-string 1)))
+              )
+          )
+      (throw (Exception. "nettikoloy"))
+
+      [(Integer. number-as-string) remainder-string])
+    )
+)
--- test/abcc/core_test.clj
+++ test/abcc/core_test.clj
@@ -5,18 +5,64 @@
 (deftest test-read-torrent
   (testing "it reads and parses a torrent file"
     (is (not (= 0 (count
-                   (read-torrent "test/ubuntu-14.04-desktop-i386.iso.torrent"
-                                ))))
+                    (read-torrent "test/ubuntu-14.04-desktop-i386.iso.torrent"
+                                  ))))
         "non zero file opened")
 
-    (is false
-        "reads the announce attribute of a torrent file")))
+    ; PENDING: clojure test doesn't have a pending :(
+    ;(is false
+    ;"reads the announce attribute of a torrent file")
+    ))
 
 (deftest test-read-bencode
-         (testing "it reads bencoded numbers"
-                  (is 123 (read-bencode "i123e"))
-                  (is -3 (read-bencode "i-3e"))
-         )
-         (testing "it reads bencoded strings"
-                  (is "super fuzzy" (read-bencode "11:super fuzzy"))
-         ))
+  (testing "it reads a properly bencoded number"
+    ;(is 123 (read-bencoded-integer "i123e"))
+    ;(is -3 (read-bencoded-integer "i-3e"))
+    )
+  (testing "it reads bencoded strings"
+    ;(is (= "digit" (read-bencode "5:digit")))
+    ;(is (= "super fuzzy" (read-bencode "11:super fuzzy")))
+    ))
+
+(deftest test-read-bencoded-integer
+  (testing
+    "it returns the number when passed a properly terminated number-substring"
+    (is (= [123 "yolokitten"] (read-bencoded-integer "123eyolokitten")))
+    (is (= [-3 ""] (read-bencoded-integer "-3e")))
+    (is (= [0 ""] (read-bencoded-integer "0e")))
+    )
+
+  (testing "it raises an exception when passed an improperly terminated number"
+    (is (thrown? Exception (read-bencoded-integer "123")))
+    (is (thrown? Exception (read-bencoded-integer "123n")))
+    (is (thrown? Exception (read-bencoded-integer "0n")))
+    (is (thrown? Exception (read-bencoded-integer "0n0")))
+    (is (thrown? Exception (read-bencoded-integer "1ne0")))
+    )
+
+  (testing "it raises an exception when passed an improperly terminated number"
+    (is (thrown? Exception (read-bencoded-integer "123")))
+    (is (thrown? Exception (read-bencoded-integer "123n")))
+    (is (thrown? Exception (read-bencoded-integer "0n")))
+    )
+
+    (testing "it raises an exception when passed an invalid digit combination"
+      (is (thrown? Exception (read-bencoded-integer "01e"))
+          "the bittorrent spec prohibits zero-prefixed nonzero numbers")
+      (is (thrown? Exception (read-bencoded-integer "001e"))
+          "the bittorrent spec prohibits zero-prefixed nonzero numbers")
+    )
+
+    (testing "it raises an exception when passed negative zero"
+      (is (thrown? Exception (read-bencoded-integer "-0e"))
+          "the bittorrent spec prohibits negative zero")
+      (is (thrown? Exception (read-bencoded-integer "-00e"))
+          "the bittorrent spec prohibits negative zero")
+      (is (thrown? Exception (read-bencoded-integer "-01e"))
+          "the bittorrent spec prohibits negative zero-prefixed numbers")
+      )
+
+  (testing "it returns the rest of the string"
+    (is (= [11 "5:hello"] (read-bencoded-integer "11e5:hello")))
+    )
+  )
