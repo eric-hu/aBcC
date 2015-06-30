62feae0e95c3209fb58bd1cac936f8255b745d87
Eric Hu <hu.eric@gmail.com>
2015-06-30 10:31:07 -0700

    Update project dependencies

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.aphyr/prism "0.1.1"]
-                 [clj-http "1.0.1"]
+                 [com.aphyr/prism "0.1.3"]
+                 [clj-http "1.1.2"]
                  ; SHA-1 library
                  [digest "1.4.4"]]
 
